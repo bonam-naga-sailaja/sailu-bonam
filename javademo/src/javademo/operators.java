c81a89c02eeb4b7a6b471d850fa7233bffde9f6f
acer <acer@TMP249-G2-M.bbrouter>
2022-06-25 16:00:59 +0530

    operators

---
+++ javademo/src/javademo/operators.java
@@ -0,0 +1,33 @@
+package javademo;
+
+public class operators 
+{
+	
+
+	public static void main(String[] args)  
+	{
+	int a = 2;
+	int b=4;
+	int c=a+b;
+	System.out.println("the value of c is:"+c);
+	int x= ++a;
+	System.out.println(x);
+	int y= b++;
+	System.out.println(y);
+	int z= --a;
+	System.out.println(z);
+	int v= a--;
+	System.out.println(v);
+	}
+}
+	
+	
+	
+	
+
+	
+
+
+	
+
+

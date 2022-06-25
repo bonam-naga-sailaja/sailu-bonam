e35f2fe085dba238ae1bfa8924dbb66b14731c6d
acer <acer@TMP249-G2-M.bbrouter>
2022-06-25 16:30:42 +0530

    integerdatatype

---
+++ javademo/src/javademo/integerdatatype.java
@@ -0,0 +1,29 @@
+package javademo;
+
+public class integerdatatype 
+{
+
+	public static void main(String[] args) 
+{ 
+	int max= 2147483647;
+	int min= -2147483648;
+	short shortMax =32767;
+	short shortMin =-32768;
+	long longMAX=9223372036854775807L;
+	long longMin=-922337203685477808L;
+	byte byteMax = 127;
+	byte byteMin = -128;
+	System.out.println(+byteMin);
+	System.out.println(+byteMax);
+	System.out.println(+longMAX);
+	System.out.println(+longMin);
+	System.out.println(+shortMax);
+	System.out.println(+shortMin);
+	
+
+
+
+
+	}
+
+}

bff45a3fac88fd09f6700fb83ea3e6cf3faa777a
acer <acer@TMP249-G2-M.bbrouter>
2022-06-25 17:10:21 +0530

    booleanunicode

---
+++ javademo/src/javademo/BooleanUnicode.java
@@ -0,0 +1,15 @@
+package javademo;
+
+public class BooleanUnicode 
+{
+
+	public static void main(String[] args) 
+	{
+		boolean var = false;
+		System.out.println(var);
+		char var1 = '\u00BD';
+		System.out.println(var1);
+
+	}
+
+}

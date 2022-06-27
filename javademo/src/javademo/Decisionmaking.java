5f0d0cc9a19a725fcfd70b7091c82a2346a93fe9
acer <acer@TMP249-G2-M.bbrouter>
2022-06-27 11:43:48 +0530

    Decisionmaking

---
+++ javademo/src/javademo/Decisionmaking.java
@@ -0,0 +1,21 @@
+package javademo;
+
+public class Decisionmaking
+{
+
+	public static void main(String[] args) 
+	{
+		int x = 5;
+		if (x!=5)
+		{
+			System.out.println("Value of x is not 5");
+		}
+		else
+		{
+			System.out.println("Value of x is 5");
+			
+		}
+
+	}
+
+}

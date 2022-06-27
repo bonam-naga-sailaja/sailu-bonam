199b4c6494eed8bb1260b5d5c7143f36d99a3ab9
acer <acer@TMP249-G2-M.bbrouter>
2022-06-27 13:15:38 +0530

    Makingdecisionwithifelse

---
+++ javademo/src/javademo/Makingdecisionwithifelse.java
@@ -0,0 +1,41 @@
+package javademo;
+
+public class Makingdecisionwithifelse
+{
+
+	public static void main(String[] args) 
+	{
+		int x=5;
+		if(x==5)
+		{
+			if(x>=5)
+			{
+				System.out.println("X is greater than 5");
+				
+			}
+			else
+			{
+
+				System.out.println("X is smaller than5");
+			
+				
+			}
+		}
+		if(x==5)
+		{
+			System.out.println("X equals to 5");
+		}
+		else if (x>5)
+		{
+			System.out.println("X is greater than 5");
+			
+			
+		}
+		else
+		{
+			System.out.println("x is smaller than 5");
+		}
+
+	}
+
+}

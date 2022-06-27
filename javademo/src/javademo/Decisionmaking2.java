252b5442c5dcd836509a2fb7ce7564656418cef8
acer <acer@TMP249-G2-M.bbrouter>
2022-06-27 11:50:15 +0530

    Decisionmaking2

---
+++ javademo/src/javademo/Decisionmaking2.java
@@ -0,0 +1,21 @@
+package javademo;
+
+public class Decisionmaking2 
+{
+
+	public static void main(String[] args) 
+	{
+		int x = 10, y=7;
+		if(x>=y)
+		{
+			System.out.println("true");
+			
+		}
+		else
+		{
+			System.out.println("false");
+		}
+
+	}
+
+}

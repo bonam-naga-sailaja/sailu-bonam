45389f24ed7de088e9e2ce3ab047117e542d5cf9
acer <acer@TMP249-G2-M.bbrouter>
2022-06-27 11:12:36 +0530

    demostring

---
+++ javademo/src/javademo/demostring.java
@@ -0,0 +1,14 @@
+package javademo;
+
+public class demostring
+{
+
+	public static void main(String[] args) 
+	{
+		String var = new String("Hello world");
+		System.out.println(var);
+		
+
+	}
+
+}

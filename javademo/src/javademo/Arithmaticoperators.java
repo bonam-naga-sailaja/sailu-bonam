44c0b16d97a3ade7c6e94068c3d793ba4846a610
acer <acer@TMP249-G2-M.bbrouter>
2022-06-27 11:32:23 +0530

    Arithmaticoperators

---
+++ javademo/src/javademo/Arithmaticoperators.java
@@ -0,0 +1,18 @@
+package javademo;
+
+public class Arithmaticoperators 
+{
+
+	public static void main(String[] args) 
+	{
+		int x = 10, y= 20;
+		System.out.println(x+y);
+		System.out.println(--x);
+		System.out.println(++y);
+		
+		
+		
+
+	}
+
+}

7b5a4731b5b75e2bc70681664942cc0e8e833cc9
acer <acer@TMP249-G2-M.bbrouter>
2022-06-27 12:12:29 +0530

    Ifelsestatement

---
+++ javademo/src/javademo/Ifelsestatement.java
@@ -0,0 +1,22 @@
+package javademo;
+
+public class Ifelsestatement 
+{
+
+	public static void main(String[] args)
+	{
+		int ageOfBoy=36;
+		int ageOfGirl=25;
+		if((ageOfBoy>=21)&&(ageOfGirl>=18))
+		{
+			System.out.println("ready to get married!");
+		}
+		else
+		{
+			System.out.println("Wait for it kiddo!");
+		}	
+		}
+
+	}
+
+

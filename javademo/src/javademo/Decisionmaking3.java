a440f02fa614530e549710db1f9ee3f691518ca5
acer <acer@TMP249-G2-M.bbrouter>
2022-06-27 12:02:01 +0530

    Decisionmaking

---
+++ javademo/src/javademo/Decisionmaking3.java
@@ -0,0 +1,19 @@
+package javademo;
+
+public class Decisionmaking3 
+{
+
+	public static void main(String[] args)
+	{
+		int x=10;int y=11;
+		if(!(x<y)||(x==y))
+		{
+			System.out.println("Condision is TRUE");
+			
+		}
+		else
+			System.out.println("Condition is FALSE");
+
+	}
+
+}

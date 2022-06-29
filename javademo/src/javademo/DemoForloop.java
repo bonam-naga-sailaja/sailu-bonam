0bdbd511b1b2cdb9e9e73e189bc9bba2a5d66c47
acer <acer@192.168.0.100>
2022-06-29 10:32:45 +0530

    Demoforloop

---
+++ javademo/src/javademo/DemoForloop.java
@@ -0,0 +1,16 @@
+package javademo;
+
+public class DemoForloop 
+{
+
+	public static void main(String[] args)
+	{
+		for(int i=1; i<=100;i++)
+		{
+			System.out.print("Value of i:");
+			System.out.println(i);
+		}
+
+	}
+
+}

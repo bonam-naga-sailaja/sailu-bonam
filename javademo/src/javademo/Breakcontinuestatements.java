3e6c7c0691e6606192d5cc3feb988d7d6db2c123
acer <acer@192.168.0.100>
2022-06-29 11:31:55 +0530

    Breakcontinuestatements

---
+++ javademo/src/javademo/Breakcontinuestatements.java
@@ -0,0 +1,22 @@
+package javademo;
+
+public class Breakcontinuestatements 
+{
+
+	public static void main(String[] args)
+	{
+		for(int i=1;i<=10;i++)
+		{
+			if(i==1)
+			{
+				continue; // break;
+				
+			}
+			System.out.println(i);
+		
+		}
+		
+
+	}
+
+}

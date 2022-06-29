31b2fe9cd4b7feef62843e0aa280984007f5c87c
acer <acer@192.168.0.100>
2022-06-29 10:49:52 +0530

    Demowhileloop

---
+++ javademo/src/javademo/Demowhileloop.java
@@ -0,0 +1,18 @@
+package javademo;
+
+public class Demowhileloop 
+{
+
+	public static void main(String[] args)
+	{
+		int i= 99;
+		while(i<=150)
+		{
+			System.out.println(i);
+			i++;
+			
+		}
+
+	}
+
+}

773c4b4563d095769b8930609000d12e82279406
acer <acer@192.168.0.100>
2022-06-29 10:42:40 +0530

    Forloop

---
+++ javademo/src/javademo/Forloop.java
@@ -0,0 +1,34 @@
+package javademo;
+
+public class Forloop 
+{
+
+	public static void main(String[] args)
+	{
+		int number = 8;
+		boolean isPrime = true;
+		for(int i=2;i<number/2;i++)
+		{
+			if(number%i==0)
+			{
+				isPrime = false;
+				
+			}
+			
+			
+		}
+		if (isPrime==true)
+		{
+			System.out.println("The number is a prime number");
+		}
+		else
+		
+		{
+			System.out.println("The number is not a prime number");
+		}
+		}
+
+
+	}
+
+

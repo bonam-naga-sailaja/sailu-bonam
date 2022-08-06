11c1d89fd72428cb6ec8c5a3a74e957921160ad4
acer <acer@192.168.0.100>
2022-08-06 16:23:04 +0530

    GssavingAcc

---
+++ javademo/src/src/Application/GsSavingAcc.java
@@ -0,0 +1,31 @@
+package Application;
+
+import FrameWork.SavingAcc;
+
+public class GsSavingAcc extends SavingAcc 
+{
+	private static final float accountbalance = 0;
+
+	public GsSavingAcc(int accno, String accname, float accBal,boolean withdraw) 
+	{
+		super(accno, accname, accBal,withdraw);
+		// TODO Auto-generated constructor stub
+	}
+
+    public void withdrow(float accBal) 
+    {
+		System.out.println("Dear custamer,your Accoutbal"+ accountbalance);
+	}
+    public void deposit(float accBal) 
+    {
+    	System.out.println("dear custamer,your acccoutbal"+ accountbalance);
+    }
+	@Override
+	public String toString() 
+	{
+		return "GsSavingAcc [accno=" + accno + ", accname=" + accname + ", accBal=" + accBal + "]";
+	}
+
+}
+
+
